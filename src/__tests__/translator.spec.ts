299fa1eb962dfaacf21e41be0f446911c20e6aae
Ondrej Bartas <ondrej@bartas.cz>
2017-03-08 12:48:00 +0100

    Add Translator

---
+++ src/__tests__/translator.spec.ts
@@ -0,0 +1,60 @@
+import { fromJS } from 'immutable';
+import { Translator, Messages } from '../translator';
+
+const locale = 'en';
+const messages = fromJS({
+  en: {
+    'Homepage headline': 'Super headline',
+    homepage: {
+      'Homepage headline': 'Super headline under scope',
+    },
+    deep: {
+      scope: {
+        'Homepage headline': 'Super headline under deep scope',
+      }
+    }
+  },
+  es: {
+    'Homepage headline': 'Super titular'
+  }
+}) as Messages;
+
+it('should return default text', () => {
+  expect(new Translator({ messages, locale }).msg('Not translated')).toEqual('Not translated');
+});
+
+it('should return text from messages', () => {
+  expect(new Translator({ messages, locale }).msg('Homepage headline')).toEqual('Super headline');
+});
+
+describe('Using scope', () => {
+  it('should return text from messages with scope', () => {
+    expect(new Translator({ messages, locale }).msg('Homepage headline', { scope: 'homepage' })).toEqual('Super headline under scope');
+  });
+
+  it('should return default text from messages with unknown scope', () => {
+    expect(new Translator({ messages, locale }).msg('Homepage headline', { scope: 'unknown' })).toEqual('Homepage headline');
+  });
+
+  it('should return text from messages with deep scope', () => {
+    expect(new Translator({ messages, locale }).msg('Homepage headline', { scope: 'deep.scope' })).toEqual('Super headline under deep scope');
+  });
+
+  it('should return object containing all messages under given key', () => {
+    expect(new Translator({ messages, locale }).msg('deep')).toEqual(messages.getIn(['en', 'deep']).toJS());
+  });
+})
+
+describe('Fallbacking locales', () => {
+  it('should return text in es from messages', () => {
+    expect(new Translator({ messages, locale: 'es' }).msg('Homepage headline')).toEqual('Super titular');
+  });
+
+  it('should return fallback text in default en from messages', () => {
+    expect(new Translator({ messages, locale: 'es' }).msg('Homepage headline', { scope: 'homepage' })).toEqual('Super headline under scope');
+  });
+
+  it('should return default text when not found in locale and defaultLocale', () => {
+    expect(new Translator({ messages, locale: 'es' }).msg('Not translated')).toEqual('Not translated');
+  });
+})
---
+++ src/translator.ts
@@ -0,0 +1,39 @@
+import { Map } from 'immutable';
+
+export type Messages = Map<string, Map<string, string>>;
+export type TranslationResult = string | number | boolean | null | object;
+
+export interface TranslatorOptions {
+  messages: Messages;
+  locale: string;
+  fallbackLocale?: string;
+}
+
+export interface MsgOptions {
+  scope: string;
+}
+
+export class Translator {
+  messages = Map() as Messages;
+  locale = 'en';
+  fallbackLocale = 'en';
+
+  constructor(options: TranslatorOptions) {
+    this.messages = options.messages;
+    this.locale = options.locale;
+    this.fallbackLocale = options.fallbackLocale || this.fallbackLocale;
+  }
+
+  // tslint:disable-next-line:typedef
+  msg = (key: string, options = {} as MsgOptions): TranslationResult => {
+    const path = options.scope ? options.scope.split('.').concat(key) : [key];
+    const result = this.__findTranslation(path) || key;
+
+    return Map.isMap(result) ? (result as Messages).toJS() : result;
+  }
+
+  __findTranslation(keys: string[]): TranslationResult | Messages {
+    return this.messages.getIn([this.locale].concat(keys)) ||
+      this.messages.getIn([this.fallbackLocale].concat(keys));
+  }
+}
