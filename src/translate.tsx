d6202e49c5d6e73f1b482a57a61a445ed6bd5e2c
Ondrej Bartas <ondrej@bartas.cz>
2017-03-28 02:38:29 +0200

    Add automatic reload of translations when state changes

--- src/translate.tsx
+++ src/translate.tsx
@@ -20,18 +20,49 @@ export interface TranslateProps {
   translateMessages: () => Messages;
 }
 
+export interface TranslateState {
+  locale: string | null;
+  messages: Messages | null;
+  fallbackLocale: string | null;
+}
+
 export type TranslateDecorator<P> = (WrappedComponent: React.ComponentClass<P & TranslateProps>) => React.ComponentClass<P>;
 
 export function translate<P>(scope?: string | string[], overrides?: MsgOptions): TranslateDecorator<P> {
   return (WrappedComponent) => {
-    return class Translate extends React.Component<P, void> {
+    return class Translate extends React.Component<P, TranslateState> {
 
       static displayName = `Translate(${WrappedComponent && (WrappedComponent.displayName || WrappedComponent.name) || 'Component'})`;
+
+      state = {
+        locale: null,
+        messages: null,
+        fallbackLocale: null
+      };
 
       static contextTypes = {
         store: React.PropTypes.object,
         translator: React.PropTypes.object
       };
+
+      componentWillMount() {
+        const { store } = this.context;
+        if (!store) return;
+
+        store.subscribe(this.updateState);
+        this.updateState();
+      }
+
+      componentWillUnmount() {
+        const { store } = this.context;
+        if (store) store.unsubscribe(this.updateState);
+      }
+
+      updateState = () => {
+        const { store } = this.context;
+        const {messages, locale, fallbackLocale} = store.getState().translate;
+        this.setState({ messages, locale, fallbackLocale });
+      }
 
       // tslint:disable-next-line:typedef
       cnt: CntFunc = (key, options = {}) => {
